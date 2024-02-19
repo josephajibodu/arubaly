4d95a25e2cc713bbb38273d57ea456b0e0feaa48
cremirdevio <josephajibodu@gmail.com>
2024-02-19 11:40:56 +0100

    fortify installed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,15 +14,14 @@
             <div class="hidden sm:mb-8 sm:flex sm:justify-center">
                 <div
                     class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                    Announcing our next round of funding. <a href="#" class="font-semibold text-indigo-600"><span
-                            class="absolute inset-0" aria-hidden="true"></span>Read more <span
-                            aria-hidden="true">&rarr;</span></a>
+                    Welcome to Arubaly.com
                 </div>
             </div>
             <div class="text-center">
-                <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Topup your Airtime and Data within seconds</h1>
-                <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui
-                    irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+                <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Your Gateway to Profitable Triangular Exchanges!</h1>
+                <p class="mt-6 text-lg leading-8 text-gray-600">
+                    Looking to make seamless transactions between Aruba Florin (AWG), USD, and Naira? Look no further! Arubaly.com offers a unique triangular exchange platform that empowers you to buy Aruba Florin, convert it to USD, and further exchange it to Naira—all with the potential for substantial profits.
+                </p>
                 <div class="mt-10 flex items-center justify-center gap-x-6">
                     <a href="#"
                        class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
