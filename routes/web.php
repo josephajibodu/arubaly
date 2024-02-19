d0f3a7de98d31faf416ac09c5757d9530c83ede2
cremirdevio <josephajibodu@gmail.com>
2024-02-19 11:02:51 +0100

    landing page route named

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('landing');
 
 Route::get('/home', function () {
     return view('home');
