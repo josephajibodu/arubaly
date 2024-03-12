8d21a11ae11171701e61df73a8b700c4a7b00d61
cremirdevio <josephajibodu@gmail.com>
2024-03-12 22:54:56 +0100

    role can be removed and added to user

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -2,11 +2,18 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\MerchantAvailability;
 use App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\Actions;
+use Filament\Infolists\Components\IconEntry;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -19,6 +26,112 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-user';
 
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Section::make('Profile Info')
+                    ->schema([
+                        TextEntry::make('email'),
+
+                        TextEntry::make('firstname'),
+
+                        TextEntry::make('lastname'),
+
+                        TextEntry::make('username'),
+
+                        TextEntry::make('phonenumber'),
+
+                        TextEntry::make('whatsappnumber'),
+                    ])->columns(2),
+
+                Section::make('Role Info')
+                    ->schema([
+                        IconEntry::make('is_user')
+                            ->label('User')
+                            ->getStateUsing(fn(User $record) => true)
+                            ->boolean(),
+
+                        IconEntry::make('Compiler')
+                            ->getStateUsing(fn(User $record) => $record->hasRole('compiler'))
+                            ->boolean(),
+
+                        IconEntry::make('Merchant')
+                            ->getStateUsing(fn(User $record) => $record->hasRole('merchant'))
+                            ->boolean(),
+
+                        IconEntry::make('Admin')
+                            ->getStateUsing(fn(User $record) => $record->hasRole('admin'))
+                            ->boolean(),
+                    ])->columns(4),
+
+                Section::make('Banking Details')
+                    ->schema([
+                        TextEntry::make('accountname'),
+
+                        TextEntry::make('bankname'),
+
+                        TextEntry::make('accountnumber'),
+                    ])->columns(2),
+
+                Section::make('Merchant Details')
+                    ->hidden(fn(User $record) => !$record->hasRole('merchant'))
+                    ->schema([
+                        TextEntry::make('rate')
+                            ->money('NGN', 100),
+
+                        TextEntry::make('min_amount')
+                            ->money('NGN', 100),
+
+                        TextEntry::make('max_amount')
+                            ->money('NGN', 100),
+
+                        TextEntry::make('payment_type'),
+
+                        TextEntry::make('terms')
+                            ->columnSpan(2),
+
+                        TextEntry::make('availability')
+                            ->icon(fn (MerchantAvailability $state): string => match ($state) {
+                                MerchantAvailability::AVAILABLE => 'heroicon-o-check-circle',
+                                MerchantAvailability::SOLDOUT => 'heroicon-o-x-mark',
+                            })
+                            ->color(fn (MerchantAvailability $state): string => match ($state) {
+                                MerchantAvailability::AVAILABLE => 'success',
+                                MerchantAvailability::SOLDOUT => 'danger',
+                            })
+                    ])->columns(2),
+
+                Actions::make([
+                    Actions\Action::make('editRoles')
+                        ->form([
+                            Forms\Components\Checkbox::make('compiler')
+                                ->default(fn(User $record) => $record->hasRole('compiler')),
+
+                            Forms\Components\Checkbox::make('merchant')
+                                ->default(fn(User $record) => $record->hasRole('merchant')),
+
+                            Forms\Components\Checkbox::make('admin')
+                                ->default(fn(User $record) => $record->hasRole('admin')),
+                        ])
+                        ->action(function (User $record, array $data) {
+                            foreach ($data as $key => $value) {
+                                if ($value) {
+                                    $record->assignRole($key);
+                                } else {
+                                    $record->removeRole($key);
+                                }
+                            }
+
+                            Notification::make()
+                                ->title('User roles updated.')
+                                ->success()
+                                ->send();
+                        })
+                ])
+            ]);
+    }
+
     public static function form(Form $form): Form
     {
         return $form
@@ -28,9 +141,11 @@ class UserResource extends Resource
                         Forms\Components\TextInput::make('firstname')
                             ->required()
                             ->maxLength(255),
+
                         Forms\Components\TextInput::make('lastname')
                             ->required()
                             ->maxLength(255),
+
                         Forms\Components\TextInput::make('username')
                             ->required()
                             ->maxLength(255),
@@ -58,6 +173,7 @@ class UserResource extends Resource
                     ])->columns(2),
 
                 Forms\Components\Section::make('Merchant Details')
+                    ->hidden(fn(User $record) => !$record->hasRole('merchant'))
                     ->schema([
                         Forms\Components\TextInput::make('rate')
                             ->required()
@@ -80,6 +196,7 @@ class UserResource extends Resource
                     ])->columns(2),
 
                 Forms\Components\Section::make('Security')
+                    ->hidden(fn() => $form->getOperation() == 'view')
                     ->schema([
                         Forms\Components\TextInput::make('password')
                             ->password()
@@ -123,6 +240,7 @@ class UserResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
@@ -145,6 +263,7 @@ class UserResource extends Resource
             'index' => Pages\ListUsers::route('/'),
             'create' => Pages\CreateUser::route('/create'),
             'edit' => Pages\EditUser::route('/{record}/edit'),
+            'view' => Pages\ViewUser::route('/{record}'),
         ];
     }
 }
