64adbf200c50c06e9b323be1c9594e50dfa340c9
cremirdevio <josephajibodu@gmail.com>
2024-03-12 23:13:27 +0100

    seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,20 +14,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $admin = User::factory()->merchant()->create([
-            'email' => 'josephajibodu@gmail.com',
-            'username' => 'cremirdevio',
-        ]);
+        if (! $admin = User::where('email', 'josephajibodu@gmail.com')->first()) {
+
+            $admin = User::factory()->merchant()->create([
+                'email' => 'josephajibodu@gmail.com',
+                'username' => 'cremirdevio',
+            ]);
+        }
 
 //        User::factory(10)->create();
 //        $merchants = User::factory(25)->merchant()->create();
 //        $compilers = User::factory(5)->merchant()->create();
 //
-//        $merchantRole = Role::create(['name' => 'merchant']);
-//        $compilerRole = Role::create(['name' => 'compiler']);
-//        Role::create(['name' => 'admin']);
+        $merchantRole = Role::firstOrCreate(['name' => 'merchant']);
+        $compilerRole = Role::firstOrCreate(['name' => 'compiler']);
+        Role::firstOrCreate(['name' => 'admin']);
 //
-//        $admin->assignRole($merchantRole);
+        $admin->assignRole($merchantRole);
 //
 //        foreach ($merchants as $merchant) {
 //            $merchant->assignRole($merchantRole);
