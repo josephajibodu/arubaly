0a5ad5d77158b40dfbd557d75ba0b76ffcb5bc7c
cremirdevio <josephajibodu@gmail.com>
2024-03-12 18:16:40 +0100

    seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,26 +19,26 @@ class DatabaseSeeder extends Seeder
             'username' => 'cremirdevio',
         ]);
 
-        User::factory(10)->create();
-        $merchants = User::factory(25)->merchant()->create();
-        $compilers = User::factory(5)->merchant()->create();
-
-        $merchantRole = Role::create(['name' => 'merchant']);
-        $compilerRole = Role::create(['name' => 'compiler']);
-        Role::create(['name' => 'admin']);
-
-        $admin->assignRole($merchantRole);
-
-        foreach ($merchants as $merchant) {
-            $merchant->assignRole($merchantRole);
-        }
-
-        foreach ($compilers as $compiler) {
-            $compiler->assignRole($compilerRole);
-        }
-
-        $this->call([
-            TransactionSeeder::class,
-        ]);
+//        User::factory(10)->create();
+//        $merchants = User::factory(25)->merchant()->create();
+//        $compilers = User::factory(5)->merchant()->create();
+//
+//        $merchantRole = Role::create(['name' => 'merchant']);
+//        $compilerRole = Role::create(['name' => 'compiler']);
+//        Role::create(['name' => 'admin']);
+//
+//        $admin->assignRole($merchantRole);
+//
+//        foreach ($merchants as $merchant) {
+//            $merchant->assignRole($merchantRole);
+//        }
+//
+//        foreach ($compilers as $compiler) {
+//            $compiler->assignRole($compilerRole);
+//        }
+//
+//        $this->call([
+//            TransactionSeeder::class,
+//        ]);
     }
 }
