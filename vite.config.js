7a4ae1da97ba91be9cf072a9e41c14aeeb7d15d0
cremirdevio <josephajibodu@gmail.com>
2024-03-18 11:27:44 +0100

    error fixed and app rebuilt

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.scss', 'resources/css/icons.scss', 'resources/js/app.js', 'resources/js/config.js'],
+            input: [
+                "resources/css/app.scss",
+                "resources/css/icons.scss",
+                "resources/js/app.js",
+                "resources/js/config.js",
+                "resources/js/alerts.js",
+            ],
             refresh: true,
         }),
     ],
