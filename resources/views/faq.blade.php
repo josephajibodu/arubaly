e7368991769ac61112e3843e3a7177eef4b36987
cremirdevio <josephajibodu@gmail.com>
2024-03-19 16:26:12 +0100

    landing page updated

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.guest')
+
+@section('title', 'Terms and Conditions')
+
+@section('content')
+    <div class="overflow-hidden bg-white py-24 sm:py-32">
+        <div class="mx-auto max-w-screen-lg px-6 lg:px-8">
+            <div class="mx-auto gap-x-8">
+                <div class="lg:pr-8 lg:pt-4">
+                    <div class="">
+                        <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Terms and Conditions</h1>
+                        <p class="mt-6 text-lg leading-8 text-gray-600">
+                            By registering for an Account or utilizing any services offered on the platform, you
+                            acknowledge your agreement to adhere to all terms and conditions outlined in this
+                            user agreement. These terms encompass a commitment to settling disputes through
+                            arbitration on an individual basis. Additionally, you commit to observing the following
+                            supplementary policies relevant to your engagement
+                        </p>
+
+                        <div>
+                            <ul class="list-disc pl-8">
+                                <li class="mb-4">Privacy Policy</li>
+                                <li class="mb-4">Acceptable Use Policy</li>
+                                <li class="mb-4">Consent to Receive Electronic Disclosures (E-Sign Disclosure and Consent)</li>
+                            </ul>
+                        </div>
+
+                        <p class="mt-6 text-lg leading-8 text-gray-600">
+                            It is important to note that we may periodically update this user agreement and the
+                            policies. The revised version will take effect upon posting, unless otherwise specified.
+                            Should any modifications reduce your rights or heighten your responsibilities, we will
+                            provide you with a minimum notice of 11 days. We retain the right to amend this
+                            agreement at any time without prior notice, in accordance with applicable law.
+                        </p>
+
+                        <p class="mt-6 text-lg leading-8 text-gray-600">
+                            By continuing to use our services following any changes to this user agreement, you
+                            express your consent to adhere to and be bound by the revised terms. If you disagree
+                            with these terms and conditions, kindly refrain from proceeding and exit the
+                            application promptly. Additionally, please be aware that we reserve the right to
+                            terminate your relationship with us if we ascertain any violation of these terms.
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
